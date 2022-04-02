2bd5e0a864a02392aa493f95d324434b0e9559b6
Panagiotis Kontos <65974766+Panoskontos@users.noreply.github.com>
2022-04-03 01:18:20 +0300

    first

--- myproject/api/urls.py
+++ myproject/api/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-     path('', apiOverview, name='overview'),
-    path('products', product_list, name='product_list')
+    path('', apiOverview, name='overview'),
+    path('products', product_list, name='product_list'),
+    path('create-product', create_product, name='create-product'),
+    path('read-product/<str:pk>', read_product, name='read_product'),
+    path('update-product/<str:pk>', update_product, name='update_product'),
+    path('delete-product/<str:pk>', delete_product, name='delete_product'),
 ]
--- myproject/api/views.py
+++ myproject/api/views.py
@@ -2,10 +2,26 @@ from django.shortcuts import render
 from django.http.response import JsonResponse
 from rest_framework.response import Response
 # Create your views here.
-from .models import Product
+from .models import *
 from .serializers import ProductSerializer
 from rest_framework.decorators import api_view
 
+
+
+@api_view(['GET'])
+def apiOverview(request):
+    api_urls = {
+        # queue
+        'Display product list':'products/',
+        'Create View':'create-product/',
+        'Detail View':'read-product/<str:pk>',
+        'Update View':'update-product/<str:pk>',
+        'Delete View':'delete-product/<str:pk>',
+        
+        '         ':'           ',
+    }
+
+    return Response(api_urls)
 
 @api_view(['GET'])
 def product_list(request):
@@ -14,14 +30,34 @@ def product_list(request):
         products_serializer = ProductSerializer(products, many=True)
         return Response(products_serializer.data)
 
-@api_view(['GET'])
-def apiOverview(request):
-    api_urls = {
-        # queue
-        
-        'Display product list':'products/',
-        
-        '         ':'           ',
-    }
 
-    return Response(api_urls)
+@api_view(['GET'])
+def read_product(request,pk):
+    if request.method == 'GET':
+        products = Product.objects.get(id=pk)
+        # many=False means return 1 object
+        products_serializer = ProductSerializer(products, many=False)
+        return Response(products_serializer.data)
+
+@api_view(['POST'])
+def create_product(request):
+    serializer = ProductSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data) 
+
+
+@api_view(['POST'])
+def update_product(request,pk):
+    product = Product.objects.get(id=pk)
+    serializer = ProductSerializer(instance=product, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data) 
+
+@api_view(['DELETE'])
+def delete_product(request,pk):
+    product = Product.objects.get(id=pk)
+    product.delete()
+    
+    return Response('Product was deleted!') 
